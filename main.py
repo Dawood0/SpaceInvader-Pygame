e1480780f7c8bab1c468d389b07430b356c2bb08
Ahmed Dawood <ahmeddawood666680@gmail.com>
2021-12-29 00:09:25 +0200

    added enemy, enemy movement and background

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ pygame.display.set_caption("space invaders")
 icon = pygame.image.load('alien.png')
 pygame.display.set_icon(icon)
 
+# setting the background
+background=pygame.image.load("19333449.jpg")
+background=pygame.transform.scale(background,(500,500))
+
 # player
 player=pygame.image.load('spaceship.png')
 player=pygame.transform.scale(player,(50,50))
@@ -26,21 +30,25 @@ playery_change=0
 # enemies
 enemy=pygame.image.load('enemy.png')
 enemy=pygame.transform.scale(enemy,(50,50))
-
-
-
+enemyx=random.randint(0,450)
+enemyy=random.randint(0,350)
+enemyx_change=0.1
+enemyy_change=0.1
 
 
 
 def Player():
     screen.blit(player,(playerx+playerx_change,playery+playery_change))       # means to draw on the screen
 
+def Enemy(x,y):
+    screen.blit(enemy,(x,y))
 
 
 # while loop for continuous running game
 game = True
 while game:
-    screen.fill((100, 200, 200))  # rgb values
+    screen.fill((100, 200, 200))        # rgb values
+    screen.blit(background,(0,0))      # setting the background
 
     # loop for closing the window when clicking the x red close button
     for event in pygame.event.get():
@@ -71,9 +79,20 @@ while game:
 
     playerx += playerx_change
     playery += playery_change
+
+    enemyx += enemyx_change
+    # enemyy += enemyy_change
+
+    # preventing the player from going out of bounds
     if playerx>screen.get_size()[0]-50 or playerx<0: playerx-=playerx_change
     if playery>screen.get_size()[1]-50 or playery<0: playery-=playery_change
 
+    # preventing enemy from going out of bounds
+    if enemyx>screen.get_size()[0]-50 or enemyx<0:
+        enemyx_change=-1*enemyx_change
+        enemyy+=10
+    if enemyy>screen.get_size()[1]-50 or enemyy<0: enemyy_change=-1*enemyy_change
 
     Player()
+    Enemy(enemyx,enemyy)
     pygame.display.update()
